1244e695c4d20714d5cf56e4468c7e53218ce298
GGalas10 <grzegorzgalas10@gmail.com>
2024-10-04 13:18:06 +0200

    Delete Angular project and and some C# code

---
+++ ApiForFirstAngular/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiForFirstAngular.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json("Testowa odpowiedź");
+        }
+    }
+}
---
+++ Infrastructure/Interfaces/IUserServices.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Interfaces
+{
+    public interface IUserServices
+    {
+        Task LoginUserAsync();
+        Task CreateUserAsync();
+        Task DeleteUserAsync();
+        Task GetUserByIdAsync(Guid id);
+    }
+}
