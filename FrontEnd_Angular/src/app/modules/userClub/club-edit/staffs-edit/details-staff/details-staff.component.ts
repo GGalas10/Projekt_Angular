4786142520ec8b75346e4bfe07e522bf311ff873
GGalas10 <grzegorzgalas10@gmail.com>
2024-12-12 16:11:51 +0100

    Add new components

---
+++ FrontEnd_Angular/src/app/modules/userClub/club-edit/staffs-edit/details-staff/details-staff.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-staff',
+  standalone: false,
+  
+  templateUrl: './details-staff.component.html',
+  styleUrl: './details-staff.component.css'
+})
+export class DetailsStaffComponent {
+
+}
