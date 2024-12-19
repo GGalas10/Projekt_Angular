f7bf326077df6aaf7efa2d19a0f1e3720915dbbe
GGalas10 <grzegorzgalas10@gmail.com>
2024-12-19 17:23:36 +0100

    Add edit staff in Angular

--- FrontEnd_Angular/src/app/modules/userClub/club-edit/staffs-edit/edit-staff/edit-staff.component.ts
+++ FrontEnd_Angular/src/app/modules/userClub/club-edit/staffs-edit/edit-staff/edit-staff.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { StaffDTO } from '../../../../../shared/Interfaces/Staff';
+import { BaseAlert } from '../../../../../shared/Component/base-alert/BaseAlertInterface';
 
 @Component({
   selector: 'app-edit-staff',
   standalone: false,
-  
+
   templateUrl: './edit-staff.component.html',
-  styleUrl: './edit-staff.component.css'
+  styleUrl: './edit-staff.component.css',
 })
 export class EditStaffComponent {
-
+  showAlert = false;
+  baseAlert: BaseAlert = {
+    Title: '',
+    Message: '',
+  };
+  staff!: StaffDTO;
+  oldStaff!: StaffDTO;
+  contractFromString = '';
+  contractToString = '';
+  @Input() staffId!: string;
+  @Output() closeEvent = new EventEmitter<void>();
+  @Output() saveEvent = new EventEmitter<StaffDTO>();
 }
