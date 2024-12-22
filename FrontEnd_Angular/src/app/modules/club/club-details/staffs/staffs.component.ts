aac8139fc926847299fbb804ad6e3ccd3b6b7267
GGalas10 <grzegorzgalas10@gmail.com>
2024-12-22 20:43:01 +0100

    Add delete btn

--- FrontEnd_Angular/src/app/modules/club/club-details/staffs/staffs.component.ts
+++ FrontEnd_Angular/src/app/modules/club/club-details/staffs/staffs.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { Staff } from '../../../../shared/Interfaces/Staff';
+import { StaffDTO } from '../../../../shared/Interfaces/Staff';
 
 @Component({
-    selector: 'app-staffs',
-    templateUrl: './staffs.component.html',
-    styleUrl: './staffs.component.css',
-    standalone: false
+  selector: 'app-staffs',
+  templateUrl: './staffs.component.html',
+  styleUrl: './staffs.component.css',
+  standalone: false,
 })
 export class StaffsComponent {
-  @Input() staffs!: Staff[];
+  @Input() staffs!: StaffDTO[];
 }
