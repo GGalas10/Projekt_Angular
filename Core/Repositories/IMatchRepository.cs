5da1ec05eba0a94c362bc13c0be6732d2ec3741e
GGalas10 <grzegorzgalas10@gmail.com>
2025-01-04 18:51:18 +0100

    Add match repository

---
+++ Core/Repositories/IMatchRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Models;
+
+namespace Core.Repositories
+{
+    public interface IMatchRepository
+    {
+        Task<List<Match>> GetAllMatches();
+        Task<List<Match>> GetAllMatchesFromLeague(Guid leagueId);
+        Task AddMatchToLeague(Match match);
+        Task<Match> GetMatchById(Guid matchId);
+    }
+}
---
+++ DataAccess/Repositories/MatchRepository.cs
@@ -0,0 +1,35 @@
+﻿using Core.Exceptions;
+using Core.Models;
+using Core.Repositories;
+using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repositories
+{
+    public class MatchRepository : IMatchRepository
+    {
+        private readonly DataDbContext _dbContext;
+        public MatchRepository(DataDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Match>> GetAllMatches()
+            => await _dbContext.Matches.AsNoTracking().ToListAsync();
+
+        public async Task<List<Match>> GetAllMatchesFromLeague(Guid leagueId)
+        {
+            var result = await _dbContext.Leagues.AsNoTracking().Include(x => x.matches).FirstOrDefaultAsync(x => x.Id == leagueId);
+            return result.matches.ToList();
+        }
+        public async Task AddMatchToLeague(Match match)
+        {
+            if (match == null)
+                throw new BadRequestException("Match_Cannot_Be_Null");
+            _dbContext.Matches.Add(match);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task<Match> GetMatchById(Guid matchId)
+            => await _dbContext.Matches.FirstOrDefaultAsync(x => x.Id == matchId);
+    }
+}
