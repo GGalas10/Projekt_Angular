381b8af839e18d875038859b83fd6c50187e1de8
GGalas10 <grzegorzgalas10@gmail.com>
2025-02-04 19:21:13 +0100

    Small changes before training

--- FrontEnd_Angular/src/app/modules/league/index/index.component.ts
+++ FrontEnd_Angular/src/app/modules/league/index/index.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LeagueDTO } from '../../../shared/Interfaces/League';
+import { LeagueService } from '../../core/Services/API/LeagueService';
 
 @Component({
   selector: 'app-index',
   standalone: false,
-  
-  templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
-})
-export class IndexComponent {
 
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css',
+})
+export class IndexComponent implements OnInit {
+  leagues!: LeagueDTO[];
+  constructor(private leagueService: LeagueService) {}
+  ngOnInit(): void {
+    this.leagueService.GetAllLeagues().subscribe({
+      next: (result) => (this.leagues = result),
+      error: (err) => console.log(err),
+    });
+  }
 }
